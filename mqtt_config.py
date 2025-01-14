568db3d55a28a01cce4dd7e8dbfe471864faa963
Vinay Sanga <vinay.sanga@aholadigital.com>
2025-01-14 13:55:05 +0100

    Test the new ini

--- mqtt_config.py
+++
@@ -1,6 +0,0 @@
-# MQTT Configuration
-BROKER = "localhost"  #broker address
-PORT = 1883
-BASE_TOPIC = "smart_city/traffic"
-ALERT_TOPIC = "smart_city/alerts"
-PUBLISH_INTERVAL = 5  # Time in seconds between messages
